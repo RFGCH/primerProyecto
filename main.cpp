ce3421fa76bee64031c25b3355ac39f1e0ce620a
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-08-28 08:28:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ int main()
     cout << "Ingrese su tercer numero a comparar:";
     cin >> c;
     
-    if (a > b){
-        if (a > c){
-            cout << a << "es el mayor";
+    if (a < b){
+        if (a < c){
+            cout << a << "es el menor";
         }
         else{
-            cout << c << "es el mayor";
+            cout << c << "es el menor";
         }
      else{
-         if (c > b){
-            cout << c << "es el mayor";
+         if (c < b){
+            cout << c << "es el menor";
          }
          else{
-            cout << b << "es el mayor";
+            cout << b << "es el menor";
          }
              
     
