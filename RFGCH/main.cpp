1cd7722f0a7c22394b585187006eed50991b3317
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-09-04 08:33:41 -0500

    Update main.cpp

--- RFGCH/main.cpp
+++ RFGCH/main.cpp
@@ -35,32 +35,65 @@ int main()
     //    cout << "es primo";
     //}
 
+
+
     // Segundo problema
 
 
 
-    int b = 100;
-    int k = 0;
-    int u = 2;
-    while(k<b){
-        while(u<k){
-            if (k%u == 0 ||  ){
-                cout << u;
-                }
-            u++;
-         }
-        if(k<u){
-            cout << u;
-            }
-        k++
+    //int b = 100;
+    //int k = 0;
+    //int u = 2;
+    //bool pr = true;
+    //while(k<b){
+    //    while(u<k){
+    //       if (k%u == 0){
+    //            pr = false;
+    //            }
+    //        u++;
+    //        }
+    //    if (!pr){
+    //        cout << k << endl;
+    //        pr = true;
+    //        }
+    //    u = 2;
+    //    k++;
+    //}
+
+    //for (b; b=0; b--)
+    //    for (int i = 2;i < b; i++){
+    //        if (b%i == 0){
+    //            pr = true
+    //            }
+    //        }
+    //    if (!pr){
+    //       cout << b << endl;
+    //        }
+    //    }
+
+
+    //Tercer problema
+    int a;
+    cin >> a;
+    int b = 2;
+    int c = 1;
+    while (b<a){
+          if(b%a == 0){
+            c = c+b;
+          }
+          b++;
     }
+    if (a==c){
+       cout << "Si cumple" << endl;
+       }
+    
 
-    for (int b = 100; b=0; b--)
-        for (int i = 2;i < b; i++){
-            if (b%i == 0){
-                cout << b << endl;
-            }
+
+    for(int n = 2; n<a ; n++){
+        if (n%a==0){
+           c = c+n
         }
-
+    if( c==a ){
+        cout
     return 0;
 }
