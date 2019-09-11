c68a14516b985a3c9e57e78e7b9b5acc6334eedd
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-09-11 10:37:55 -0500

    Add files via upload

---
+++ MenorDeNumeros/main.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void separarDigitos(int a){
+ int b = a/10000;
+ int c = (a=a-(10000*b))/1000;
+ int d = (a=a-(1000*c))/100;
+ int e = (a=a-(100*d))/10;
+ a = a-(10*e);
+ cout<< b << "\t" << c << "\t" << d << "\t" << e << "\t" << a << endl;
+}
+int digitos(int a){
+ int b=0;
+  for (int i = 1;i<a;i=i*10){
+   b++;
+  }
+  return b;
+ }
+bool palindromo(int a){
+ int b = a/1000;
+ a = a - b * 1000;
+ a = a - 100 * (a/100);
+ if(b%11==0 && b!=a){
+
+  return false;
+ }
+ else{
+    if((b+a)%11==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+ }
+}
+int fibonacci(int a){
+ int b;
+ int c = 1;
+ int d = 0;
+
+ for(int i=1;i<=a;i++){
+  b=d+c;
+  d=c;
+  c=b;
+ }
+ return c;
+}
+bool caracter(char a){
+
+    int m = static_cast<int>(a);
+    if(m > 47 && m < 58){
+       return true;
+    }
+    else{
+        return false;
+    }
+}
+bool caracterVisceversa(char a){
+
+    int m = static_cast<int>(a);
+
+    if(m >= 65 && m <= 90){
+        cout << "Su minuscula es: " <<  static_cast<char>(m+32) << endl;
+        return true;
+    }
+    if(m >= 97 && m <=122){
+        cout << "Su mayuscula es: " << static_cast<char>(m-32) << endl;
+        return true;
+    }
+    return false;
+}
+
+
+int main()
+{
+    //Primer ej
+    cout << "##Separar los digitos de un numero##" << endl;
+    int a;
+    cout << "Ingresar un numero de 5 digitos: " ;
+    cin >> a;
+    separarDigitos(a);
+
+    //Segundo ej
+    cout << "##Hallar la cantidad de digitos##" << endl;
+    int b;
+    cout << "Ingresar un numero: " ;
+    cin >> b;
+    b = digitos(b);
+    cout << "El numero de digitos es " << b << endl;
+
+    //Tercer ej
+    cout << "##Identificar si un numero de 5 digitos es palindromo##" << endl;
+    int c;
+    cout << "Ingrese un numero de 5 digitos: " ;
+    cin >> c;
+    c = palindromo(c);
+    if (c){
+        cout << "Es palindromo" << endl;
+    }
+    else {
+        cout << "No es palindromo" << endl;
+    }
+
+    //Cuarto ej
+    cout << "##Hallar el numero correspondiente en la serie de fibonacci" << endl;
+    int d;
+    cout << "Ingresar el numero de correspondecia en la serie de fibonacci: ";
+    cin >> d;
+    d = fibonacci(d);
+    cout << "El numero correspondiente es " << d << endl;
+
+    //Quinto ej
+    cout <<"##Identificar si el valor ingresado es un numero##" << endl;
+    int e;
+    cout <<"Ingrese un valor: " ;
+    cin >> e;
+    if (e){
+        cout << "Es numero" << endl;
+    }
+    else {
+        cout << "No es numero" << endl;
+    }
+
+    //Sexto ej
+    cout <<"##Cambiar una mayuscula a minuscula y visceversa e identificar si es o no un caracter##" << endl;
+    char f;
+    bool g;
+    cout <<"Ingrese un valor: " ;
+    cin >> f;
+    g = caracterVisceversa(f);
+    if (!g){
+        cout << "Es numero" << endl;
+    }
+
+
+    return 0;
+}
