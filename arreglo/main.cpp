737830394bb354fe48a2c76a253f425793183a20
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-09-25 11:13:59 -0500

    Update main.cpp

--- arreglo/main.cpp
+++ arreglo/main.cpp
@@ -12,22 +12,22 @@ void swap(int &a,int &b){
 
 void print(int arr[],int t){
 
-    while(t)
-        cout << arr[--t] << endl;
+    while(t--)
+        cout << arr[t] << endl;
 
 }
 
 int suma(int arr[],int t){
 
     int c=0;
-    while(t--){
+    while(t--)
         c += arr[t];
-    }
     return c;
+
 }
 
 int sumar(int arr[],int t){
-    
+
     if (t==1)
         return arr[t-1];
     return arr[t-1] += sumar(arr,t-1);
@@ -36,81 +36,71 @@ int sumar(int arr[],int t){
 
 void inv(int arr[], int t){
 
-    for(int i=0;i<t/2;i++){
+    for(int i=0;i<t/2;i++)
         swap(arr[--t],arr[i]);
-    }
-    
+
 }
 
-int invr(int arr[], int t,int i=0){
+void invr(int arr[], int t,int i=0){
 
-    int v=t/2;
-    if (v==0){
-        swap(arr[t-1],arr[i]);
-        return 0;
-    }
-    invr(arr,t-1,i+1);
-    return 0;
-
+    if (!(t/2))
+        return swap(arr[t-1],arr[i]);
+    return invr(arr,t-1,i+1);
 
 }
 
 void burbuja(int arr[],int t){
 
-    for (int i = 1;i<t;i++){
-        for(int j = 0;j<t-i;j++){
-            if (arr[j]>arr[j+1])
+    for(int i=1;i<t;i++)
+        for(int j=0;j<t-i;j++)
+            if(arr[j]>arr[j+1])
                 swap(arr[j],arr[j+1]);
-        }
-    }
 
 }
 
 void insercion(int arr[],int t){
 
     int c,a;
-    for (int i = 1; i<t;i++){
+    for(int i=1;i<t;i++){
         c=arr[i];
         a=i-1;
-        while(a >=0 && arr[a]>c){
-            arr[a+1]=arr[a];
-            a--;
-        }
+        while(a>=0 && arr[a]>c)
+            arr[a+1]=arr[a--];
         arr[a+1]=c;
     }
 
 }
 
-int particion(int arr[],int min,int max){
+int particion(int arr[],int max,int min){
+
     int p=arr[max];
-    int i=(min-1);
-    for(int j=min;j <=max-1;j++){
-        if(arr[j]<p){
-            i++;
-            swap(arr[i],arr[j]);
-        }
-    }
+    int i=min-1;
+
+    for(int j=i+1;j<=max-1;j++)
+        if(arr[j]<p)
+            swap(arr[++i],arr[j]);
+
     swap(arr[i+1],arr[max]);
     return i+1;
+
 }
-void quicksort(int arr[],int min,int max){
+
+void quicksort(int arr[],int max,int min = 0){
 
     if(min < max){
-        int p = particion(arr,min,max);
-        quicksort(arr,min,p-1);
-        quicksort(arr,p+1,max);
+        int p = particion(arr,max,min);
+        quicksort(arr,p-1,min);
+        quicksort(arr,max,p+1);
     }
-
 
 }
 
 int main()
 {
 
-    int arr[]={10,5,9,4,3,6,21,3,7,35,23};
-    print(arr,11);
-    quicksort(arr,0,11);
-    print(arr,11);
-
+    int arr[]={10,5,9,3,2,4};
+    print(arr,6);
+    insercion(arr,6);
+    print(arr,6);
     return 0;
 }
