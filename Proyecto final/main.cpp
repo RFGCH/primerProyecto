0d2fe6b3e49f606f3881c6c9154494af083ab9fb
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-10-25 11:21:46 -0500

    Add files via upload

--- Proyecto final/main.cpp
+++ Proyecto final/main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 string registro(){
 
     string nick,pass,registro;
-    cout << "Menu" << endl;
+    cout << "\n\t-------Ingreso------\n" << endl;
     cout << "Nick: ";
     getline(cin,nick);
     nick = "users//"+nick+".txt";
 
     ifstream ingresar(nick.c_str());
     if(!ingresar){
-        cout << "No esta registrado" << endl;
+        cout << "No esta registrado\n" << endl;
         return "0";
     }
     else{
@@ -24,14 +24,14 @@ string registro(){
             getline(cin,pass);
             getline(ingresar,registro);
             if(registro == pass){
-                cout << "Ingreasando..." << endl;
+                cout << "Ingreasando...\n" << endl;
                 return nick;
             }
             if(pass == "0"){
                 exit = 0;
                 return "0";
             }
-            cout << "Su clave es errada" << endl;
+            cout << "Su clave es errada\n" << endl;
 
         }
     }
@@ -40,6 +40,7 @@ string registro(){
 void registrarse(){
 
     string nick,clave,user;
+    cout << "\n\t------Registro------\n" << endl;
     cout << "Ingrese su nick: ";
     getline(cin,nick);
     cout << "Ingrese su clave: ";
@@ -47,33 +48,27 @@ void registrarse(){
     nick = "users//"+nick+".txt";
     ofstream registrar(nick.c_str());
     registrar << clave;
+    clave+="\n";
     registrar.close();
-
+    cout << "Registrado con exito\n" << endl;
 }
 
+
+
+void editar(string &a,string &b){
+    rename(a.c_str(),b.c_str());
+}
+void print(string nick){
+    string asd;
+    ifstream asf(nick.c_str());
+    getline(asf,asd);
+}
 int main()
 {
-    //ofstream f("Registro.txt");
-    //f << "hola mundo" << endl;
-   //f << "asfxvag"<< endl;
-    //f << "he there"<< endl;
-    //f.close();
-    //cout << "Dime el nombre del fichero: ";
-    //string nombre;
-    //getline(cin, nombre);
 
-    //ifstream fichero(nombre.c_str());
-    //string linea;
-    //int x = 1;
-    //while (x--)
-    //getline(fichero,linea);
-    //cout << "Se ha leido: " << endl;
-    //cout << linea << endl;
-    //fichero.close();
 
     registro();
     registrarse();
-
 
     return 0;
 }
