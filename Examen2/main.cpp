83fe7318f31b374f56738d5df97533b7cffa7912
RFGCH <54367628+RFGCH@users.noreply.github.com>
2019-11-04 00:02:34 -0500

    Add files via upload

--- Examen2/main.cpp
+++ Examen2/main.cpp
@@ -31,55 +31,91 @@ class Empleado{
         void reduccion10porc(){
             salario -= salario/10;
         }
-        void salarioanual(){
-            cout << salario*12 << endl;
+        int salarioanual(){
+            int aux = salario*12;
+            return aux;
         }
         void print(){
-            cout << "---"<< nombre << "---" << endl;
+            cout << "\n---"<< nombre << "---\n" << endl;
             cout << apellido << endl;
             cout << salario << endl;
         }
 
 };
-void cambio(Empleado *a,Empleado *b){
-    Empleado *aux;
-    *aux = *a;
-    *a = *b;
-    *b = *aux;
+void cambio(Empleado a,Empleado b){
+    Empleado aux;
+    aux = a;
+    a = b;
+    b = aux;
 }
-void ordenamiento(Empleado *a, int n){
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < n-i ; j++){
-            if( a[j].getapellido() > a[j+1].getapellido()){
-                cambio(a+j,a+j+1);
-            }
+void burbuja(Empleado *arr,int t){
+    for(int i=1;i<t;i++){
+        int c=0;
+        Empleado *pnt=arr+1;
+        for(int j=0;j<t-i;j++){
+            if(arr->getapellido() > pnt->getapellido())
+                swap(*arr,*pnt);
+            arr++;
+            pnt++;
+            c++;
         }
+        arr-=c;
     }
 }
 
 int main()
 {
-    int alita;
-    Empleado Paco("Paco","Ruperto",8000);
-    Empleado Manolo("Manolo","Geronimo",7000);
-    cout << "---Salario---" << endl ;
-    cout << Paco.getsalario() << endl;
-    cout << Manolo.getsalario() << endl;
-    Paco.reduccion10porc();
-    Manolo.reduccion10porc();
-    cout << "---Reduccion del 10%---" << endl;
-    cout << Paco.getsalario() << endl;
-    cout << Manolo.getsalario() << endl;
-    cout << "---Salario anual---" << endl;
-    Paco.salarioanual();
-    Manolo.salarioanual();
-    Empleado a[2]={Paco,Manolo};
-    a[0].print();
-    a[1].print();
-    ordenamiento(a,2);
-    a[0].print();
-    a[1].print();
-    cin >> alita;
-
+    cout << "\nExame 1." << endl;
+    cout << "\n1)Empleado" << endl;
+    cout << "\nDeclarando un empleado sin argumentos" << endl;
+    Empleado Roberto;
+    Roberto.print();
+    cout << "\nDeclarando dos empleados con argumentos" << endl;
+    Empleado Joseph("Joseph","Ancofk",8000);
+    Empleado Ricardo("Ricardo","Betmhefr",9000);
+    Joseph.print();
+    Ricardo.print();
+    cout << "\nSea el empleado Joseph: " << endl;
+    cout << "\nSu nombre es: ";
+    cout << Joseph.getnombre() << endl;
+    cout << "Su apellido es: ";
+    cout << Joseph.getapellido() << endl;
+    cout << "Su salario es: ";
+    cout << Joseph.getsalario() << endl;
+    cout << "\nAutoajuste de un empleado con salario negativo" << endl;
+    int salario_negativo = -1234;
+    Empleado Hidalgo("Hidalgo","Cristous",salario_negativo);
+    Hidalgo.print();
+    cout << "\nMostrando el salario anual de Joseph y Ricardo: " << endl;
+    cout << "\n--Joseph--\n" << endl;
+    cout << Joseph.salarioanual() << endl;
+    cout << "\n--Ricardo--\n" << endl;
+    cout << Ricardo.salarioanual() << endl;
+    cout << "\nBajando el sueldo en 10% a Joseph y Ricardo\n" << endl;
+    Joseph.reduccion10porc();
+    Ricardo.reduccion10porc();
+    cout << Joseph.getsalario() << endl;
+    cout << Ricardo.getsalario() << endl;
+    cout << "\nSalario aunal de todos los empleados: " << endl;
+    cout << "\nDe Joseph: ";
+    cout << Joseph.salarioanual() << endl;
+    cout << "De Ricardo: ";
+    cout << Ricardo.salarioanual() << endl;
+    cout << "De Roberto: ";
+    cout << Roberto.salarioanual() << endl;
+    cout << "De Hidalgo: ";
+    cout << Hidalgo.salarioanual() << endl;
+    cout << "2)Algoritmo de ordenamiento" << endl;
+    cout << "\nUsando el algoritmo burbuja" << endl;
+    cout << "-----Antes-----" << endl;
+    Empleado a[4]={Ricardo,Joseph,Roberto,Hidalgo};
+    for(int i=0;i<4;i++){
+        a[i].print();
+    }
+    cout << "-----Despues-----" << endl;
+    burbuja(a,4);
+    for(int i=0;i<4;i++){
+        a[i].print();
+    }
     return 0;
 }
